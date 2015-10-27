22d70980a45913f71d42d90cd14748138d03eac2
Micaela <micamenara@gmail.com>
2015-10-27 15:39:24 -0300

    Update bikes.php

--- VM/public/Day1/bikes.php
+++ VM/public/Day1/bikes.php
@@ -1,26 +1,3 @@
 <?php
 
-class Bikes
-{
- 
-    public $speed_var = '80 Km/h';
-    public $canFly_var ="It can't fly";
-    public $maxPassengers_var ='2';
-
-
-    public function speed() {
-        echo $this->speed_var;
-        echo "<br>";
-    }
-    public function canFly() {
-        echo $this->canFly_var;
-        echo "<br>";
-    }
-    public function maxPassengers() {
-        echo $this->maxPassengers_var;
-        echo "<br>";
-    }
-
-}
-
 ?>
