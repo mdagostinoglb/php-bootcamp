be2557c8b660ba86ccd2321da856988ee2850ff6
Micaela <micamenara@gmail.com>
2015-10-27 15:40:23 -0300

    Update planes.php

--- VM/public/Day1/planes.php
+++ VM/public/Day1/planes.php
@@ -1,26 +1,3 @@
 <?php
-class Planes
-{
-    // Declaración de propiedades
- 
-    public $speed_var = '900 Km/h';
-    public $canFly_var ='It can fly';
-    public $maxPassengers_var ='650';
 
-    // Declaración de métodos
-
-    public function speed() {
-        echo $this->speed_var;
-        echo "<br>";
-    }
-    public function canFly() {
-        echo $this->canFly_var;
-        echo "<br>";
-    }
-    public function maxPassengers() {
-        echo $this->maxPassengers_var;
-        echo "<br>";
-    }
-
-}
 ?>
