35d07c305ab1bbf1840540dcefed05d6a336cf58
Micaela <micamenara@gmail.com>
2015-10-27 15:49:49 -0300

    Update cars.php

--- VM/public/Day1/cars.php
+++ VM/public/Day1/cars.php
@@ -1,3 +1,25 @@
 <?php
 
+class Cars
+{
+ 
+    public $speed_var = '200 Km/h';
+    public $canFly_var ="It can't fly";
+    public $maxPassengers_var ='5';
+
+
+    public function speed() {
+        echo $this->speed_var;
+        echo "<br>";
+    }
+    public function canFly() {
+        echo $this->canFly_var;
+        echo "<br>";
+    }
+    public function maxPassengers() {
+        echo $this->maxPassengers_var;
+        echo "<br>";
+    }
+
+}
 ?>
