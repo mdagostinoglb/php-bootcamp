52938d95b2467e8aaf0a7439eb16077de2bb50aa
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-27 14:31:24 -0300

    Create bikes.php

---
+++ VM/public/Day2/bikes.php
@@ -0,0 +1,53 @@
+<?php
+	
+	class Bikes{
+		public function name(){
+			if (get_class($this) == "Planes"){
+				echo "Planes";
+			}
+			elseif (get_class($this) == "Cars"){
+				echo "Cars";
+			}
+			elseif (get_class($this) == "Bikes"){
+				echo "Bikes";
+			}
+		}
+	
+		public function speed() {
+			if (get_class($this) == "Planes"){
+				return 900;
+			}
+			elseif (get_class($this) == "Cars"){
+				return 110;
+			}
+			elseif (get_class($this) == "Bikes"){
+				return 30;
+			}
+		}
+		
+		public function canFly(){
+			if (get_class($this) == "Planes"){
+				echo "It can fly. ";
+			}
+			elseif (get_class($this) == "Cars"){
+				echo "It can not fly. ";
+			}
+			elseif (get_class($this) == "Bikes"){
+				echo "It can not fly. ";
+			}
+		}
+		
+		public function maxPassengers(){
+			if (get_class($this) == "Planes"){
+				return 600;
+			}
+			elseif (get_class($this) == "Cars"){
+				return 6;
+			}
+			elseif (get_class($this) == "Bikes"){
+				return 3;
+			}
+		}
+		
+	}
+?>
