b27e97b5c36e9677075dcb1a7639b67dc9487f32
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-27 19:24:16 -0300

    Update planes.php

--- VM/public/Day1/planes.php
+++ VM/public/Day1/planes.php
@@ -2,51 +2,19 @@
 	
 	class Planes{
 		public function name(){
-			if (get_class($this) == "Planes"){
-				echo "Planes";
-			}
-			elseif (get_class($this) == "Cars"){
-				echo "Cars";
-			}
-			elseif (get_class($this) == "Bikes"){
-				echo "Bikes";
-			}
+			echo "Planes";
 		}
 	
 		public function speed() {
-			if (get_class($this) == "Planes"){
-				return 900;
-			}
-			elseif (get_class($this) == "Cars"){
-				return 110;
-			}
-			elseif (get_class($this) == "Bikes"){
-				return 30;
-			}
+			return 900;
 		}
 		
 		public function canFly(){
-			if (get_class($this) == "Planes"){
-				echo "It can fly. ";
-			}
-			elseif (get_class($this) == "Cars"){
-				echo "It can not fly. ";
-			}
-			elseif (get_class($this) == "Bikes"){
-				echo "It can not fly. ";
-			}
+			echo "It can fly";
 		}
 		
 		public function maxPassengers(){
-			if (get_class($this) == "Planes"){
-				return 600;
-			}
-			elseif (get_class($this) == "Cars"){
-				return 6;
-			}
-			elseif (get_class($this) == "Bikes"){
-				return 3;
-			}
+			return 600;
 		}
 		
 	}
