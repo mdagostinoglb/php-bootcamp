f06e31852316501f36bd82508af75e07d38c4b43
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-28 13:57:31 -0300

    Update index.php

--- VM/public/Day3/index.php
+++ VM/public/Day3/index.php
@@ -1,3 +1,150 @@
 <?php
+	abstract class Transport{
+		abstract protected function distance();
+		abstract protected function name();
+		abstract protected function speed();
+		abstract protected function canFly();
+		abstract protected function maxPassengers();	
+	}
+	
+	class Planes extends Transport{
+		public function distance(){
+			if (empty($_GET['hours'])) { $hours="";} else { $hours=filter_var($_GET['hours'], FILTER_SANITIZE_NUMBER_INT);}
+			return $hours;
+		}
+		
+		public function name(){
+			echo "Planes";
+		}
+	
+		public function speed() {
+			return 900;
+		}
+		
+		public function canFly(){
+			echo "It can fly";
+		}
+		
+		public function maxPassengers(){
+			return 600;
+		}
+	}
+	
+	class Cars extends Transport{
+		public function distance(){
+			if (empty($_GET['hours'])) { $hours="";} else { $hours=filter_var($_GET['hours'], FILTER_SANITIZE_NUMBER_INT);}
+			return $hours;
+		}
+		
+		public function name(){
+			echo "Cars";
+			}
+		
+	
+		public function speed(){
+			return 110;
+		}
+		
+		public function canFly(){
+			echo "It can not fly";
+		}
+		
+		public function maxPassengers(){
+			return 6;
+		}
+		
+	}
+	
+	class Bikes extends Transport{
+		public function distance(){
+			if (empty($_GET['hours'])) { $hours="";} else { $hours=filter_var($_GET['hours'], FILTER_SANITIZE_NUMBER_INT);}
+			return $hours;
+		}
+		
+		public function name(){
+			echo "Bikes";
+		}
+	
+		public function speed() {
+			return 30;
+		}
+		
+		public function canFly(){
+			echo "It can not fly";
+		}
+		
+		public function maxPassengers(){
+			return 3;
+		}
+		
+	}
+	
+	$one=array(new Planes,new Cars(),new Bikes());
+	
+	$i=0;
+	while ($i<3){
+		$obj1 = $one[$i];
+		$obj1->name();
+		echo ": Speed: ";
+		echo $obj1->speed();
+		echo ". ";
+		$obj1->canFly();
+		echo ". Max Passengers: ";
+		echo $obj1->maxPassengers();
+		echo ".";
+		echo "<br>";
+		$i=$i+1;
+	}
+	
+	echo "<br><br><br>";
+	
+	echo "
+	<form action=\"three.php\" method=\"GET\">
+		Choose a vehicle:
+		<br>
+		<input type=\"radio\" name=\"vehicle\" value=\"1\" checked>Plane
+		<br>
+		<input type=\"radio\" name=\"vehicle\" value=\"2\">Car
+		<br>
+		<input type=\"radio\" name=\"vehicle\" value=\"3\">Bike
+		<br>
+		Choose hours: 
+		<br>
+		<input type=\"text\" name=\"hours\" size=\"15\" value=\"1\">
+		<br>
+		<input type=\"submit\" value=\"Calculate Distance\">
 
-// Use this file for Exercise of day 3. You may need to create other files too.
+	</form>
+	";
+	if (empty($_GET['vehicle'])) { $vehicle="";} else { $vehicle=$_GET['vehicle'];}
+	
+	if ($vehicle == "1"){
+		$nobj1= new Planes();
+		$maxspeed=$nobj1->speed();
+		$resultado=$maxspeed*$nobj1->distance();
+		echo "<div style=\"color:#FF0404; background-color:#EEBE3D; width:250px; border: solid; padding:4px;    \" class=\"alert alert-danger\">
+			You can travel ".$resultado."Km. in ".$nobj1->distance()." hours if you choose a Plane.
+			</div>";
+		//echo $resultado;
+	}
+	
+	if ($vehicle == "2"){
+		$nobj1= new Cars();
+		$maxspeed=$nobj1->speed();
+		$resultado=$maxspeed*$nobj1->distance();
+		echo "<div style=\"color:#FF0404; background-color:#EEBE3D; width:250px; border: solid; padding:4px;    \" class=\"alert alert-danger\">
+			You can travel ".$resultado."Km. in ".$nobj1->distance()." hours if you choose a Car.
+			</div>";
+		//echo $resultado;
+	}
+	
+	if ($vehicle == "3"){
+		$nobj1= new Bikes();
+		$maxspeed=$nobj1->speed();
+		$resultado=$maxspeed*$nobj1->distance();
+		echo "<div style=\"color:#FF0404; background-color:#EEBE3D; width:250px; border: solid; padding:4px;    \" class=\"alert alert-danger\">
+			You can travel ".$resultado."Km. in ".$nobj1->distance()." hours if you choose a Bike.
+			</div>";
+		//echo $resultado;
+	}
+?>
