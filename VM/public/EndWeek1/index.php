2754299d3b871583defa59abff0cea36167fdfd8
micamenara <micamenara@gmail.com>
2015-10-29 19:41:07 -0300

    commit

--- VM/public/EndWeek1/index.php
+++ VM/public/EndWeek1/index.php
@@ -1,3 +1,21 @@
-<?php
 
-// Use this file for Exercise of day 4 and 5. You may need to create other files too.
+<?php 
+//include ("check_conection.php");
+
+//Datos de conexión a la base de datos
+$host = '127.0.0.1';
+$db = 'bookstore';
+$user = 'root';
+$pass = 'root';
+$DBH = new PDO("mysql:host=$host;dbname=$db", $user, $pass); 
+$STH = $DBH->query('SELECT title, price from books');
+$STH->setFetchMode(PDO::FETCH_ASSOC); 
+$i='1';
+while($row = $STH->fetch()) {
+  $var[$i]= $row['title'];
+  echo $var[$i].'<br>';
+    $i++;
+  }   
+
+$DBH = null;
+?>
