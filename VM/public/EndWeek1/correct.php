67e07e7bd92b9a662cb78a2d7d7db3f6fa10a173
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-29 10:51:33 -0300

    Create correct.php

---
+++ VM/public/EndWeek1/correct.php
@@ -0,0 +1,17 @@
+<?php
+	$ntitle=mysql_real_escape_string($_GET['ntitle']);
+	$nprice=filter_var($_GET['nprice'], FILTER_VALIDATE_FLOAT);
+	$ndesc=mysql_real_escape_string($_GET['ndesc']);
+	
+	$link = mysql_connect('localhost', 'root', '') or die('No se pudo conectar al servidor: ' . mysql_error());
+	mysql_select_db('books') or die('No se pudo seleccionar la base de datos');
+	mysql_query("SET NAMES 'utf8'");
+	$query="INSERT INTO `books`(`title`,`price`,`description`) VALUES ('$ntitle','$nprice','$ndesc')";
+	$result = mysql_query($query) or die('Consulta fallida: ' . mysql_error());
+	if($result){
+		echo "Insertado.";
+	}
+	mysql_close($link);
+	echo "<br><br>";
+	echo "<a href='index.php'>Back</a>";
+?>
