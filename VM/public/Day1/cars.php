c28a57223c2431ada4187ceb6677a675b305be82
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 16:11:28 -0300

    Update cars.php

--- VM/public/Day1/cars.php
+++ VM/public/Day1/cars.php
@@ -1,22 +1,21 @@
 <?php
-	
-	class Cars{
-		public function name(){
-			echo "Cars";
-			}
-		
-	
-		public function speed(){
-			return 110;
-		}
-		
-		public function canFly(){
-			echo "It can not fly";
-		}
-		
-		public function maxPassengers(){
-			return 6;
-		}
-		
-	}
+    class Cars
+    {
+        public function name()
+        {
+            echo "Cars";
+        } 
+		public function speed()
+        {
+            return 110;
+        } 
+		public function canFly()
+        {
+            echo "It can not fly";
+        } 
+		public function maxPassengers()
+        {
+            return 6;
+        }
+    }
 ?>
