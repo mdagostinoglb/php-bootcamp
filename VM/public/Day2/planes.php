e366193c360fde5f403e3e7ded885b65c444b17d
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 16:13:11 -0300

    Update planes.php

--- VM/public/Day2/planes.php
+++ VM/public/Day2/planes.php
@@ -1,21 +1,21 @@
 <?php
-	
-	class Planes{
-		public function name(){
-			echo "Planes";
-		}
-	
-		public function speed() {
-			return 900;
-		}
-		
-		public function canFly(){
-			echo "It can fly";
-		}
-		
-		public function maxPassengers(){
-			return 600;
-		}
-		
-	}
+    class Planes
+    {
+        public function name()
+        {
+            echo "Planes";
+        } 
+		public function speed()
+        {
+            return 900;
+        } 
+		public function canFly()
+        {
+            echo "It can fly";
+        } 
+		public function maxPassengers()
+        {
+            return 600;
+        }
+    }
 ?>
