a4adf00743ee00c8073c1fc59df82ea6d2f969f9
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 16:51:37 -0300

    Update planes.php

--- VM/public/Day3/planes.php
+++ VM/public/Day3/planes.php
@@ -1,11 +1,11 @@
-<?php
+<?php	
     class Planes extends Transport implements theFunctions
     {
         public function distance()
         {
-            if(empty($_GET['hours'])) { 
+            if(empty($_GET['hours'])) {
                 $hours="";
-            } else { 
+            } else {
                 $hours=filter_var($_GET['hours'], FILTER_VALIDATE_FLOAT);
             }
             return $hours;
@@ -20,14 +20,16 @@
         {
             return $speed=900;
         }
+
         public function canFly()
         {
-            echo $canfly= "It can fly";
+            echo $canfly="It can fly";
         }
 		
         public function maxPassengers()
         {
             return $maxpassengers=600;
         }
+		
     }
 ?>
