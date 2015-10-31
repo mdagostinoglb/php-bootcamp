3b3eb15d7b6632934d23175bc833659f4ce744bc
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 15:58:41 -0300

    Update bikes.php

--- VM/public/Day1/bikes.php
+++ VM/public/Day1/bikes.php
@@ -1,21 +1,22 @@
 <?php
-	
-	class Bikes{
-		public function name(){
-			echo "Bikes";
-		}
-	
-		public function speed() {
-			return 30;
-		}
-		
-		public function canFly(){
-			echo "It can not fly";
-		}
-		
-		public function maxPassengers(){
-			return 3;
-		}
-		
-	}
+    class Bikes
+    {
+        public function name()
+        {
+            echo "Bikes";
+        }
+        public function speed()
+        {
+            return 30;
+        }
+        public function canFly()
+        {
+            echo "It can not fly";
+        }
+        public function maxPassengers()
+        {
+            return 3;
+        }
+    }
+
 ?>
