20917f326d28e2aa89bc96c6fb6775d048158ae9
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 16:33:00 -0300

    Update bikes.php

--- VM/public/Day3/bikes.php
+++ VM/public/Day3/bikes.php
@@ -1,25 +1,34 @@
 <?php	
-	class Bikes extends Transport implements theFunctions{
-		public function distance(){
-			if(empty($_GET['hours'])) { $hours="";} else { $hours=filter_var($_GET['hours'], FILTER_VALIDATE_FLOAT);}
-			return $hours;
-		}
+    class Bikes extends Transport implements theFunctions
+    {
+        public function distance()
+        {
+            if (empty($_GET['hours'])) { 
+                $hours="";
+            } else { 
+                $hours=filter_var($_GET['hours'], FILTER_VALIDATE_FLOAT);
+            }
+            return $hours;
+        }
 		
-		public function name(){
-			echo $name="Bikes";
-		}
+        public function name()
+        {
+            echo $name="Bikes";
+        }
 	
-		public function speed() {
-			return $speed=30;
-		}
+        public function speed() 
+        {
+            return $speed=30;
+        }
+
+        public function canFly()
+        {
+            echo $canfly="It can not fly";
+        }
 		
-		public function canFly(){
-			echo $canfly="It can not fly";
-		}
-		
-		public function maxPassengers(){
-			return $maxpassengers=3;
-		}
-		
-	}
+        public function maxPassengers()
+        {
+            return $maxpassengers=3;
+        }
+    }
 ?>
