38e6a0785c1671d71fccbce58fb033f12b277880
nicolasrafaelli <nico-nbb@hotmail.com>
2015-10-31 16:48:42 -0300

    Update transport.php

--- VM/public/Day3/transport.php
+++ VM/public/Day3/transport.php
@@ -1,20 +1,29 @@
 <?php
-	abstract class Transport implements theFunctions{
-		public function distance(){
-			if (empty($_GET['hours'])){ $hours="";} else { $hours=filter_var($_GET['hours'], FILTER_VALIDATE_FLOAT);}
-			return $hours;
-		}
-		public function name(){
-			echo $this->$name;
-		}
-		public function speed(){
-			return $this->$speed;
-		}
-		public function canFly(){
-			return $this->$canfly;
-		}
-		public function maxPassengers(){
-			return $this->$maxpassengers;
-		}
-	}
+    abstract class Transport implements theFunctions
+    {
+        public function distance()
+        {
+            return $this->$hours;
+        }
+        
+		public function name()
+        {
+            echo $this->$name;
+        }
+        
+        public function speed()
+        {
+            return $this->$speed;
+        }
+		
+        public function canFly()
+        {
+            return $this->$canfly;
+        }
+		
+        public function maxPassengers()
+        {
+            return $this->$maxpassengers;
+        }
+    }
 ?>
