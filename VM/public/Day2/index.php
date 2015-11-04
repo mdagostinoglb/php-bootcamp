cac5bc1b710be6b7f9e6ccfb98d23d698626e260
Agufarre <af_17_92@hotmail.com>
2015-11-04 03:19:34 -0300

    Update index.php

--- VM/public/Day2/index.php
+++ VM/public/Day2/index.php
@@ -1,3 +1,23 @@
 <?php
+<html>
+
+<form method="POST" action="val.php">
+			<p>
+				<label for="options">Travel using a</label><br />
+		        <input type="radio" name="enviar" value="Plane"> Plane </input><br />
+		        <input type="radio" name="enviar" value="Car"> Car </input><br />
+		        <input type="radio" name="enviar" value="Bike"> Bike </input><br />
+			</p>
+			<p>
+				<label for="passengers">Passengers</label><br />
+		        <input type="text" name="passengers"/>
+			<p>
+
+		    <input type="submit" name="submit" value="Travel!"></input>
+		</form>
+	</div>
+</html>
+
+
 
 // Use this file for Exercise of day 2. You may need to create other files too.
