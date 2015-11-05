17a69075577225e573e70a26b09725fba9c247de
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-05 17:35:14 -0300

    Create database.php

---
+++ VM/public/EndWeek2/database.php
@@ -0,0 +1,22 @@
+<?php
+	$host="localhost"; 
+	$dbname="books"; 
+	$user="root"; 
+	$pass="";
+	try{
+		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+		$STH = $DBH->query('SELECT * FROM `books`');
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		echo "<center><table>";
+		echo "<th align=center>Titles</th>";
+		while($row = $STH->fetch()) {
+			echo "<tr><td align=center>";
+			echo "<a href=\"index.php/libro/".$row['id']."\">".$row['title']."</a>";
+			echo "</td></tr>";
+		}
+		echo "</table></center>";
+	}
+	catch(PDOException $e) {
+		echo $e->getMessage();
+	 }
+?>
