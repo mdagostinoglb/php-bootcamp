67f6acdd0f64fd26381cb30143cf64e64228d1d2
Ignacio <imchiodo@hotmail.com>
2015-11-05 05:30:30 -0300

    Day6
    Completed Day6 exercise

--- VM/public/Day6/index.php
+++ VM/public/Day6/index.php
@@ -1,3 +1,17 @@
 <?php
+require 'vendor/autoload.php';
 
-// Use this file for Exercise of day 6. You may need to create other files too.
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
+
+$yaml = new Parser();
+
+try {
+    $array1 = $yaml->parse(file_get_contents('books.yml'));
+} catch (ParseException $e) {
+    echo ("Unable to parse the YAML string: %s" . $e->getMessage());
+}
+
+$loader = new Twig_Loader_Filesystem('templatefolder');
+$twig = new Twig_Environment($loader);
+echo $twig->render('template.html', $array1);
