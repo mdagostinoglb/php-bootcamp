28ad1fe05cb5a7f7d29cee03dbb5aa68099c133b
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-13 14:57:25 -0300

    Create 2015_11_10_220550_create_bookings_table.php

---
+++ VM/public/Week3/laravel/database/migrations/2015_11_10_220550_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('room_id')
+				->unsigned()
+				->nullable();
+            $table->foreign('room_id')
+                ->references('id')->on('rooms')
+                ->onDelete('cascade');
+			$table->date('day_of_arrive');
+			$table->date('day_of_leaving');
+			$table->string('personal_ids');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bookings');
+    }
+}
