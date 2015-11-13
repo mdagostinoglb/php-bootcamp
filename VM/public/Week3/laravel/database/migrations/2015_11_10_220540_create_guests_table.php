6955b0ba89a14cdf2e6401f619d25128a59dc64f
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-13 14:56:59 -0300

    Create 2015_11_10_220540_create_guests_table.php

---
+++ VM/public/Week3/laravel/database/migrations/2015_11_10_220540_create_guests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('surname');
+			$table->string('name');
+			$table->integer('age');
+			$table->integer('room_id')
+				->unsigned()
+				->nullable();
+            $table->foreign('room_id')
+                ->references('id')->on('rooms')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('guests');
+    }
+}
