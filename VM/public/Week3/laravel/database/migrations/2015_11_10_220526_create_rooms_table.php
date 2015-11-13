c8db33384139c626f1c80103d9c7a0e057d3ed8e
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-13 14:56:33 -0300

    Create 2015_11_10_220526_create_rooms_table.php

---
+++ VM/public/Week3/laravel/database/migrations/2015_11_10_220526_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('room_beds');
+			$table->integer('room_floor');
+			$table->decimal('price_per_night');
+			$table->boolean('free');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rooms');
+    }
+}
