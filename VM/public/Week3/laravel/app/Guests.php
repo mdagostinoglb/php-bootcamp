72d7efe58cbdc03eb110c98cfb1b3e6fdc13659e
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-16 15:55:27 -0300

    Update Guests.php

--- VM/public/Week3/laravel/app/Guests.php
+++ VM/public/Week3/laravel/app/Guests.php
@@ -4,5 +4,16 @@ use Illuminate\Database\Eloquent\Model;
 
     class Guests extends Model
     {
-        protected $fillable = ['surname', 'name', 'age', 'room_id'];
+        protected $table = "guests";
+	    protected $fillable = ['surname', 'name', 'age', 'room_id'];
+	
+        public function room()
+        {
+            return $this->belongsTo('week3\Rooms')
+        }
+	
+	    public function bookings()
+        {
+            return $this->hasMany('week3\Bookings')
+        }
     }
