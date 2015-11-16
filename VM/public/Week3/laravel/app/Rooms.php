87327ab5fdb07d3acb91f73d36796b20ec10fae8
nicolasrafaelli <nico-nbb@hotmail.com>
2015-11-16 15:56:18 -0300

    Update Rooms.php

--- VM/public/Week3/laravel/app/Rooms.php
+++ VM/public/Week3/laravel/app/Rooms.php
@@ -4,5 +4,16 @@ use Illuminate\Database\Eloquent\Model;
 
     class Rooms extends Model
     {
-        protected $fillable = ['room_beds', 'room_floor', 'price_per_night', 'free'];
+        protected $table = "rooms";
+    	protected $fillable = ['room_beds', 'room_floor', 'price_per_night', 'free'];
+	
+	    public function guests()
+        {
+            return $this->hasMany('week3\Guests')
+        }
+	
+	    public function bookings()
+        {
+            return $this->hasMany('week3\Bookings')
+        }
     }
